e78a337009b21096e0cd5a2ed33a8b37ee92e7d3
newalvaro9 <alvaropobladoresteban9@gmail.com>
2022-05-26 19:14:57 +0200

    API for saving into Database

---
+++ routes/saveGroup.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const groupsDB = require('./../database/models/groups.js')
+
+router.post('/', async (req, res) => {
+    const { name, mate1, mate2, mate3, mate4, mate5, mate6 } = req.body;
+    console.log(req.body);
+    if (!name || !req.body || Object.keys(req.body).length === 0) return res.json({ "groupName": name, "code": "400", "success": "no", "info": "Body is empty" });
+
+    let isOnDB = await groupsDB.findOne({ groupName: name });
+
+    if (!isOnDB) {  //Save to db
+        await groupsDB.create({
+            groupName: name,
+            mate1: mate1,
+            mate2: mate2,
+            mate3: mate3,
+            mate4: mate4,
+            mate5: mate5,
+            mate6: mate6
+        });
+        res.json({ "groupName": name, "code": "201", "success": "yes", "info": "Successfully created" });
+    } else { // No save bcs is on db
+        res.json({ "groupName": name, "code": "400", "success": "no", "info": "Group Already Exist" });
+    }
+})
+
+/*
+groupName
+countryCode
+timeToFinish
+*/
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,19 +2,21 @@ const app = require('express')()
 const cors = require('cors');
 require('./database/database.js')
 
+
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'http://localhost:5000'
 }))
 
-app.use(require("express").urlencoded({extended: false}));
+app.use(require("express").urlencoded({extended: true}));
+app.use(require("express").json());
 
 /* Dirs */
 const mainRoute = require('./routes/main');
-const existUserRoute = require('./routes/existUser')
+const saveGroupRoute = require('./routes/saveGroup')
 
 /* Route Creation */
 app.use('/', mainRoute)
-app.use('/existUser', existUserRoute)
+app.use('/saveGroup', saveGroupRoute)
 
 app.listen(5000, () => {
   console.log('Server on port 5000');
