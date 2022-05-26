2e379afc36b31280891f4f5ae9a948c6423d4c64
newalvaro9 <alvaropobladoresteban9@gmail.com>
2022-05-26 19:14:03 +0200

    Added teammates

--- database/models/groups.js
+++ database/models/groups.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 const groupsSchema = new mongoose.Schema({
   groupName: { type: String, required: true },
-  countryCode: {type: String, required: true},
-  timeToFinish: {type: String, default: "15:00"}
+  countryCode: { type: String },
+  timeToFinish: { type: String, default: "15:00" },
+  mate1: { type: String, default: null },
+  mate2: { type: String, default: null },
+  mate3: { type: String, default: null },
+  mate4: { type: String, default: null },
+  mate5: { type: String, default: null },
+  mate6: { type: String, default: null }
 })
 
 module.exports = mongoose.model('groups', groupsSchema)
