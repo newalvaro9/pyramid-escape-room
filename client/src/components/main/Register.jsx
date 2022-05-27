decdf221e936af491c700f5c8bf46a8ffcc1040b
newalvaro9 <alvaropobladoresteban9@gmail.com>
2022-05-27 19:35:36 +0200

    Add more mates 2/3

--- client/src/components/main/Register.jsx
+++ client/src/components/main/Register.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import $ from "jquery"
+import Errors from "../Errors";
 
 export default function Register({ setShowch }) {
+    const [error, setError] = useState();
+
+    
 
     function register() {
+        const namegroup = document.getElementById('useRefGroupName').value;
+        const teammate1 = document.getElementById('useRefTeammate1').value;
+        const teammate2 = document.getElementById('useRefTeammate2').value || "no";
+        const teammate3 = document.getElementById('useRefTeammate3').value || "no";
+        const teammate4 = document.getElementById('useRefTeammate4').value || "no";
+        const teammate5 = document.getElementById('useRefTeammate5').value || "no";
+        const teammate6 = document.getElementById('useRefTeammate6').value || "no";
+
+        // obligatories
+        if (!namegroup) {
+            return setError('❌ ¡Escribe un nombre para tu grupo!')
+        }
+        if (!teammate1 || teammate1.length < 2) {
+            return setError('❌ ¡Escribe el nombre de al menos un integrante!')
+        }
+
+        //optionals
+
+
         setShowch("renderInfo")
     }
-    var num = 2
-
+    var num = 1
+    // Add more teammates
     function newMate() {
         if (num === 5) {
             $("#add-more-mates").attr('hidden', '')
@@ -38,28 +61,24 @@ export default function Register({ setShowch }) {
                 </div>
             </div>
             <br />
+            <Errors error={error} setError={setError} />
             <div id="newDivs">
                 <div className="input-info">
-                    <p style={{ "marginBottom": "5px" }}>Nombre del grupo</p>
-                    <input type="text" name="teamname" className="form-control" maxlength="120" required />
+                    <p style={{ "marginBottom": "5px" }}>Nombre del grupo <span className="red-color">*</span></p>
+                    <input type="text" id="useRefGroupName" className="form-control" maxlength="50" required />
                 </div>
                 <br />
                 <div className="input-info">
-                    <p style={{ "marginBottom": "5px" }}>Integrante 1 (Nombre y apellidos)</p>
-                    <input type="text" name="teammate1" id="teammate1" className="form-control" maxlength="100" required />
-                </div>
-                <br />
-                <div className="input-info">
-                    <p style={{ "marginBottom": "5px" }}>Integrante 2 (Nombre y apellidos)</p>
-                    <input type="text" name="teammate2" id="teammate2" className="form-control" maxlength="100" required />
+                    <p style={{ "marginBottom": "5px" }}>Integrante 1 (Nombre y apellidos) <span className="red-color">*</span></p>
+                    <input type="text" id="useRefTeammate1" className="form-control" maxlength="50" required />
                 </div>
                 <br />
             </div>
             <div className="new-mates-and-reg">
                 <input id="add-more-mates" className="add-more-mates" type="image" src="/images/mas.png" onClick={newMate}></input>
                 <button className="btn btn-primary register-btn btn-custom-css" type="button" onClick={register}>Registrarme</button>
-                <br />
             </div>
+            <br />
         </>
     )
 }
