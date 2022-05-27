a0f3bf79ff885369c9fa79301fd23acce5d13ced
newalvaro9 <alvaropobladoresteban9@gmail.com>
2022-05-27 19:34:25 +0200

    Changed default time to null

--- database/models/groups.js
+++ database/models/groups.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const groupsSchema = new mongoose.Schema({
   groupName: { type: String, required: true },
   countryCode: { type: String },
-  timeToFinish: { type: String, default: "15:00" },
+  timeToFinish: { type: String, default: null },
   mate1: { type: String, default: null },
   mate2: { type: String, default: null },
   mate3: { type: String, default: null },
