690b3c8e08dc4e56a6682eded10235c4235a90af
newalvaro9 <alvaropobladoresteban9@gmail.com>
2022-05-28 16:21:38 +0200

    Button for registering

--- client/src/components/success/Final.jsx
+++ client/src/components/success/Final.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import $ from "jquery"
 
-export default function Final({setShowch}) {
+export default function Final({ setShowch, registered }) {
 
     function goRegister() {
         setShowch("renderRegister")
@@ -9,6 +9,9 @@ export default function Final({setShowch}) {
 
     function final() {
         $("#div-confeti").addClass("confeti");
+        if (registered) {
+            $("#goRegister").hide()
+        }
     }
 
     useEffect(() => {
@@ -32,27 +35,29 @@ export default function Final({setShowch}) {
                                 <h3 className='card-title'>Clasificación</h3>
                             </div>
                             <div className='btn-rg-on-top'>
-                                <button className="btn btn-primary btn-rg-on-top" type="button" onClick={goRegister}>Registrarme</button>
+                                <button className="btn btn-primary btn-rg-on-top" id="goRegister" type="button" onClick={goRegister}>Registrarme</button>
                             </div>
                         </div>
                         <div className='card-body' style={{ overflow: "auto" }}>
                             <table>
-                                <tr>
-                                    <th>Nombre del grupo</th>
-                                    <th>Tiempo</th>
-                                </tr>
-                                <tr>
-                                    <td>AnnA Hompajarera</td>
-                                    <td>12:56 min.</td>
-                                </tr>
-                                <tr>
-                                    <td>Professors 🤣</td>
-                                    <td>14:16 min.</td>
-                                </tr>
-                                <tr>
-                                    <td>Fitipaldis</td>
-                                    <td>09:04 min.</td>
-                                </tr>
+                                <tbody>
+                                    <tr>
+                                        <th>Nombre del grupo</th>
+                                        <th>Tiempo</th>
+                                    </tr>
+                                    <tr>
+                                        <td>AnnA Hompajarera</td>
+                                        <td>12:56 min.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Professors 🤣</td>
+                                        <td>14:16 min.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Fitipaldis</td>
+                                        <td>09:04 min.</td>
+                                    </tr>.y                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+                                </tbody>
                             </table>
                         </div>
                     </div>
