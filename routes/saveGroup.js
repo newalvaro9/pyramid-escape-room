9fd25e99d49f88aca34eaedf70f843192959e4de
newalvaro9 <alvaropobladoresteban9@gmail.com>
2022-05-28 16:21:04 +0200

    Added time to db

--- routes/saveGroup.js
+++ routes/saveGroup.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const groupsDB = require('./../database/models/groups.js')
 
 router.post('/', async (req, res) => {
-    const { name, mate1, mate2, mate3, mate4, mate5, mate6 } = req.body;
+    const { name, mate1, mate2, mate3, mate4, mate5, mate6, time } = req.body;
     console.log(req.body);
     if (!name || !req.body || Object.keys(req.body).length === 0) return res.json({ "groupName": name, "code": "400", "success": "no", "info": "Body is empty" });
 
@@ -11,6 +11,8 @@ router.post('/', async (req, res) => {
     if (!isOnDB) {  //Save to db
         await groupsDB.create({
             groupName: name,
+            timeToFinish: time,
+            isTime: true,
             mate1: mate1,
             mate2: mate2,
             mate3: mate3,
