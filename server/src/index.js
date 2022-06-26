ae80f92fd9739e054732336eab0834704851d099
newalvaro9 <alvaropobladoresteban9@gmail.com>
2022-06-26 12:21:54 +0200

    Cors origin deleted

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,9 @@ const app = require('express')()
 const cors = require('cors');
 require('../database/database.js')
 
-
-app.use(cors({
-  origin: 'http://localhost:3000'
-}))
-
 app.use(require("express").urlencoded({extended: true}));
 app.use(require("express").json());
-
+app.use(cors())
 /* Dirs */
 const mainRoute = require('./routes/main');
 const saveGroupRoute = require('./routes/saveGroup')
